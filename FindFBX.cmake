92a4357202460f9df5c80a9385b99fac824dca93
Lars Bilke <lars.bilke@ufz.de>
2012-06-11 18:17:11 +0200

    Better windows search path.

--- FindFBX.cmake
+++ FindFBX.cmake
@@ -17,7 +17,7 @@ set(FBX_MAC_LOCATIONS
 )
 
 set(FBX_WIN_LOCATIONS
-    "C:/Program Files/Autodesk/FBX/FbxSdk/2013.1"
+    "$ENV{ProgramFiles}/Autodesk/FBX/FbxSdk/2013.1"
 )
 
 set(FBX_SEARCH_LOCATIONS

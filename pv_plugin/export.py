fee264766fe1120e1348dd2dbf306d4de3825fce
Lars Bilke <lars.bilke@ufz.de>
2014-03-17 16:04:39 +0100

    Correctly export multiple objects.

--- pv_plugin/export.py
+++ pv_plugin/export.py
@@ -1,5 +1,6 @@
 #author: Carolin Helbig 2013-07-23, Lars Bilke 2014-03-17
 import os
+import PyQt4.QtGui
 try: paraview.simple
 except: from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
@@ -11,18 +12,24 @@ objects = GetSources()
 
 # Open a directory dialog in users home directory
 # For Windows binaries see http://www.riverbankcomputing.com/software/pyqt/download
-import PyQt4.QtGui
 dir = str(PyQt4.QtGui.QFileDialog.getExistingDirectory(None, "Select a directory", PyQt4.QtCore.QDir.home().canonicalPath()))
 
 for tuple, object in objects.items():
+	SetActiveView(view)
 	name = tuple[0]
 	index = tuple[1]
 	#print '{0} has id {1}'.format(name, index)
 	displayProperties = GetDisplayProperties(object)
 	if not displayProperties.Visibility:
 		continue
+	renderView = CreateRenderView()
+	Show(object, renderView)
+
 	print 'Exporting {0}'.format(name)
 	filename = os.path.join(dir, '{0}.fbx'.format(name))
+
 	exporter = exporters.FbxExporter(FileName = filename)
-	exporter.SetView(view)
+	exporter.SetView(renderView)
 	exporter.Write()
+
+	Delete(renderView)
